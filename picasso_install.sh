179b2d3e3af41e8222c9637981f5f1876cd5fd99
jipeng <peng.ji@dianping.com>
2017-02-22 16:03:57 +0800

    check if folder exists

--- picasso_install.sh
+++ picasso_install.sh
@@ -1,3 +1,4 @@
+cd ~
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 export NVM_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node
 source ~/.nvm/nvm.sh
@@ -5,5 +6,9 @@ nvm install v6.9.5
 ~/.nvm/versions/node/v6.9.5/bin/npm install -g nrm --registry=https://registry.npm.taobao.org
 ~/.nvm/versions/node/v6.9.5/bin/nrm use taobao
 ~/.nvm/versions/node/v6.9.5/bin/npm install -g typescript
+
+if [ -d picasso-server ]; then
+	rm -rf picasso-server
+fi
 git clone git@code.dianpingoa.com:mobile/picasso-server.git
 cd picasso-server && ~/.nvm/versions/node/v6.9.5/bin/npm link
